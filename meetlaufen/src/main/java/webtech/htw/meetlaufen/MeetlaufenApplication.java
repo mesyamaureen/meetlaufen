cdc40493a1745f47df3effaf57c818042b278436
mesyamaureen <mesyamaureen@gmail.com>
2023-04-26 10:10:28 +0200

    spring app erstellt

---
+++ meetlaufen/src/main/java/webtech/htw/meetlaufen/MeetlaufenApplication.java
@@ -0,0 +1,13 @@
+package webtech.htw.meetlaufen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetlaufenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetlaufenApplication.class, args);
+	}
+
+}
---
+++ meetlaufen/src/test/java/webtech/htw/meetlaufen/MeetlaufenApplicationTests.java
@@ -0,0 +1,13 @@
+package webtech.htw.meetlaufen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetlaufenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
