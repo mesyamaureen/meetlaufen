7dc896255152379484e27da47e168519357e8ebe
mesyamaureen <mesyamaureen@gmail.com>
2023-04-26 08:55:51 +0200

    spring boot app generiert

---
+++ meetlaufen-app/src/test/java/group14/meetlaufen/MeetlaufenApplicationTests.java
@@ -0,0 +1,13 @@
+package group14.meetlaufen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetlaufenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
